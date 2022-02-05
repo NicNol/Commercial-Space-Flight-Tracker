3256b9d64a0f5887ac99c28a3f72902208e1ba44
AloofSkip <nic.veloce@gmail.com>
2022-02-04 18:03:10 -0800

    fix navbar component paths

--- src/components/navbar/navbarMenuToggle.jsx
+++ src/components/navbar/navbarMenuToggle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box, Icon } from "@chakra-ui/react";
 import { IoClose, IoMenu } from "react-icons/io5";
 
-export default function MenuToggle({ toggle, isOpen }) {
+export default function NavbarMenuToggle({ toggle, isOpen }) {
     return (
         <Box display={"block"} onClick={toggle} py={2} px={4}>
             <Icon as={isOpen ? IoClose : IoMenu} color={"#333"} h={8} w={8} />
