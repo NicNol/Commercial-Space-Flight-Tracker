8fdc26697b387f15bcad06fec64c926b4888cb72
AloofSkip <nic.veloce@gmail.com>
2022-02-07 17:00:05 -0800

    Add catch block to existing api routes

--- api/controllers/commonController.js
+++ api/controllers/commonController.js
@@ -3,6 +3,10 @@ const execute = require("../../util/mysql").execute;
 
 exports.getAll = async function (req, res) {
     const tableName = req.params[0];
-    const result = await execute(`SELECT * FROM ${tableName}`);
-    res.json(result);
+    try {
+        const result = await execute(`SELECT * FROM ${tableName}`);
+        res.json(result);
+    } catch (err) {
+        res.status(404).json({ Error: err });
+    }
 };
